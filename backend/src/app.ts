47ffbb8486bfcf2d0775169adb76b9ad260327ad
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2026-06-05 15:12:08 +0100

    setting up the frontend and the backend files to be ready for building

---
+++ backend/src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express()
+
+
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }))
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "API is running"
+    })
+})
+
+export default app
